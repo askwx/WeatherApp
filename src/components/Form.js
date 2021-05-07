f65bd7f4b11f1cbbc6b8f421bb5826cb044e9bb9
Asuka <asuka@tlu.ee>
2021-05-07 22:08:32 +0300

    form API

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,20 @@
 import{useState}from "react";
-
+import axios from "axios";
 
 
 const Form = () =>{
     const[city,setCity] = useState("");
+    const getWeather =(e) =>{
+        e.preventDefault();
+        axios.get("http://api.weatherapi.com/v1/current.json?key=0c82c4c554114dce92f114137210705&q=London&aqi=no")
+        .then(res =>console.log(res))
+    }
 
     return(
 
         <form>
             <input type = "text" name="city" placeholder="cityname" onChange={e => setCity(e.target.value)}/>
-            {city}<button type = "submit">Get weather</button>
+            <button type = "submit" onClick ={getWeather}>Get weather</button>
         </form>
 
     );
