b6234ff64370cfe21cd8a9d97815b3dc2e0d8240
Asuka <asuka@tlu.ee>
2021-05-11 21:06:05 +0300

    props2

--- src/App.js
+++ src/App.js
@@ -10,17 +10,32 @@ import './App.css';
 
 function App() {
   const[city,setCity] = useState("");
+  const[results,setResults] = useState({    
+    country:"",
+    cityName:"",
+    temperature:"",
+    couditionText:"",
+    icon:""
+  });
   const getWeather =(e) =>{
       e.preventDefault();
       axios.get("http://api.weatherapi.com/v1/current.json?key=0c82c4c554114dce92f114137210705&q=London&aqi=no")
-      .then(res =>console.log(res))
+      .then(res =>{
+        setResults({
+          country:res.data.location.country,
+          cityName:res.data.location.name,
+          temperature:res.data.current_temp_c,
+          couditionText:res.data.current.condition.text,
+          icon:res.data.current.condition.icon
+        })
+      })
   }
 
   return (
     <div className="App">
      <Title />
      <Form setCity = {setCity} getWeather = {getWeather} />
-     <Results />
+     <Results result = {results} />
     </div>
   );
 }
